f06adea73a550e790337b5bcb521b454832a74e3
Mateus Trindade <mateus_2607@hotmail.com>
2021-08-08 19:08:47 -0300

    Novas adequações ao heroku

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY', '&cq$fz&ifa!2fr6)+#vid*$t*fw#$w01146x9ewgj2-@1v=t')
+    'DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
