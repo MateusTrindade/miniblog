98f53231cd2a75b1e4e1b0f58ec6494031f89b29
Mateus Trindade <mateus_2607@hotmail.com>
2021-08-08 19:17:25 -0300

    Novas adequações ao heroku

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['damp-harbor-65864.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['damp-harbor-65864.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
